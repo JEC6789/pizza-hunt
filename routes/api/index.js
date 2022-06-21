7ec6dd8eaccccc464b8ee7b087ee31dd64a7d984
Jonathan Cushing <firex42069@gmail.com>
2022-06-20 22:35:27 -0400

    comment functionality

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+const commentRoutes = require("./comment-routes.js");
 const pizzaRoutes = require("./pizza-routes.js");
 
+router.use("/comments", commentRoutes);
 router.use("/pizzas", pizzaRoutes);
 
 module.exports = router;
